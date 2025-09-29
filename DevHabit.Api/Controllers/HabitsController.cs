f7c83585f83e841db5458ceb41eb48ec787155bd
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-09-29 23:25:57 +0500

    Реализация Delete для Habit

--- DevHabit.Api/Controllers/HabitsController.cs
+++ DevHabit.Api/Controllers/HabitsController.cs
@@ -86,4 +86,18 @@ public sealed class HabitsController(ApplicationDbContext dbContext) : Controlle
         await dbContext.SaveChangesAsync();
         return NoContent();
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteHabit(string id)
+    {
+        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+        if (habit is null)
+        {
+            return NotFound();
+        }
+
+        dbContext.Habits.Remove(habit);
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
 }
