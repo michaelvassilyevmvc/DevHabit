23d8e11f9799329f9f5adf430734cb8db817471a
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-06 12:03:09 +0500

    Ref. Remove unusing link

--- DevHabit.Api/Program.cs
+++ DevHabit.Api/Program.cs
@@ -1,20 +1,5 @@
 using DevHabit.Api;
-using DevHabit.Api.Database;
-using DevHabit.Api.DTOs.Habits;
-using DevHabit.Api.Enities;
 using DevHabit.Api.Extensions;
-using DevHabit.Api.Middleware;
-using DevHabit.Api.Services;
-using DevHabit.Api.Services.Sorting;
-using FluentValidation;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Newtonsoft.Json.Serialization;
-using Npgsql;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
